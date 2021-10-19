406e4187709ce04d56e679841d2a6884d87bd0a1
Александр Шкаликов <ash@almanitrus.ru>
2021-10-19 15:54:24 +0300

    add section3

--- components/mainpage/section3/Section3.vue
+++ components/mainpage/section3/Section3.vue
@@ -3,18 +3,24 @@
 		<div class="row services"
 		v-for="(el,i) in servicesList" :key="i">
 
-				<div class="col-4">
+				<div class="col-lg-4">
 					<div class="title"
 					:class="el.iconClass">
 						{{el.title}}
 						<div class="subtitle">{{el.subtitle}}</div>
 					</div>
 				</div>
-				<div class="col-6">
-					<div class="desc" v-html="el.subtitle"></div>
+				<div class="col-lg-6">
+					<div class="desc" v-html="el.descHhtml"></div>
 				</div>
-				<div class="col-2">
-					<div class="cost">от {{el.price}}&#8381;</div>
+				<div class="col-lg-2">
+					<div class="price">
+						<div class="value">
+							от
+							<span>{{parseInt(el.price).toLocaleString()}}&#8381;</span>
+						</div>
+						<button></button>
+					</div>
 				</div>
 
 		</div>
